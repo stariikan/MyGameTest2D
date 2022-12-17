f1f0800a6078cbaf93f3d51c65a2d67076cfc3de
stariikan <buycomp751@gmail.com>
2022-12-17 11:54:54 +0400

    add behavior to entity script
    add behavior to entity script

--- Assets/C#Scripts/Enemy_Skelet.cs
+++ Assets/C#Scripts/Enemy_Skelet.cs
@@ -2,27 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Skelet : Entity
+public class Enemy_Skelet : Entity //наследование класса сущности (то есть методы которые используются в Entity будут применены и к этому обьекту)
 {
     // Start is called before the first frame update
     [SerializeField] private int hp = 30; //жизни скелета
+    [SerializeField] private float speed = 3.5f; //параметр скорости скелета
+    private SpriteRenderer sprite;
+    private void Start()
+    {
+        dir = transform.right;//начальное направление движения 
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject == Hero.Instance.gameObject) //Если скелет соприкасается именно с героем 
                                                               //(тут получается ссылка на скрипт Hero и оттуда берется gameObject)
         {
             Hero.Instance.GetDamage(); //Из скрипта Hero вызывается публичный метод который меняет переменную hp -= 10.
-            hp -= 10; //но при этом и у скелета тратятся 10 жизней
+            //hp -= 10; //но при этом и у скелета тратятся 10 жизней
             Debug.Log("Скелет потерял 10 жизней, осталось" + hp);//написание в логах количества жизней у скелета
         }
 
         if (hp < 0)//если hp меньше или равно 0
             Die();//то смерть и уничтожение gameObject, это публичный метод из скрипта Entity  
     }
-
-    // Update is called once per frame
-    void Update()
+    
+// Update is called once per frame
+void Update()
     {
-        
+        Move();
     }
 }
--- Assets/C#Scripts/Entity.cs
+++ Assets/C#Scripts/Entity.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector3 dir;
+    private bool flipRight = true; //Поворот спрайта на право, состояние = правда, нужно для поворота спрайта во время смены движения
     public virtual void GetDamage()
     {
         
     }
+    public void Flip() //Тут мы создаем метод Flip при вызове которого спрайт меняет направление
+    {
+        flipRight = !flipRight; //Когда запускается метод Flip переменная flipRight меняется на false
+        Vector3 theScale = transform.localScale; //получение масштаб объекта
+        theScale.x *= -1;//тут происходит переворот изображения например 140 меняется на -140 тем самым полностью измени направление спрайта (картинка отзеркаливается)
+        transform.localScale = theScale; //Масштаб преобразования относительно родительского объекта GameObjects
+    }
+    public void Move() //создаем публичный метом Move
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f); //проверка доступности направления
+                                                                                                                                              //смена этого направления
 
-    // Update is called once per frame
+        if (colliders.Length > -3) dir *= -1f;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);//и двигать самого монстра
+        if (dir.x > 0 && !flipRight) //если движение больше нуля и произшло flipRight =не true то нужно вызвать метод Flip (поворот спрайта)
+        {
+            Flip();
+        }
+        else if (dir.x < 0 && flipRight) //если движение больше нуля и произшло flipRight = true то нужно вызвать метод Flip (поворот спрайта)
+        {
+            Flip();
+        }
+    }
     public virtual void Die() //Обьявляем публичный метод Die
     {
         Destroy(this.gameObject);//уничтожить этот игровой обьект
